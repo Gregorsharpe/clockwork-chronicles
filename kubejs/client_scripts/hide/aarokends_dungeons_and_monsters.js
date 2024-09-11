cbfe1d9123704341c6d76e51d92fbe8e6053abaa
gregorsharpe <gregorbsharpe@gmail.com>
2024-09-11 18:57:26 -0400

    Remove aarokend.

--- kubejs/client_scripts/hide/aarokends_dungeons_and_monsters.js
+++
@@ -1,6 +0,0 @@
-JEIEvents.hideItems(event => {
-  event.hide('aarokends_dungeons_and_monsters:backpack')
-  event.hide('aarokends_dungeons_and_monsters:sins_wand')
-  event.hide('aarokends_dungeons_and_monsters:jkjk')
-  event.hide('aarokends_dungeons_and_monsters:matell')
-})
--- kubejs/server_scripts/remove/aarokends_dungeons_and_monsters.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes(event => {
-  event.remove({ output: 'aarokends_dungeons_and_monsters:backpack' })
-  event.remove({ output: 'aarokends_dungeons_and_monsters:sins_wand' })
-  event.remove({ output: 'aarokends_dungeons_and_monsters:jkjk' })
-  event.remove({ output: 'aarokends_dungeons_and_monsters:matell' })
-})
