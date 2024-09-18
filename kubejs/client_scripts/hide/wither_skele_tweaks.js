0d27cb968d02120c508562d9d8d27cd4931217ac
gregorsharpe <gregorbsharpe@gmail.com>
2024-09-18 18:38:55 -0400

    Disable WST swords for being too strong.

---
+++ kubejs/client_scripts/hide/wither_skele_tweaks.js
@@ -0,0 +1,4 @@
+JEIEvents.hideItems(event => {
+  event.hide('wstweaks:lava_blade')
+  event.hide('wstweaks:blaze_blade')
+})
---
+++ kubejs/server_scripts/remove/wither_skele_tweaks.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(event => {
+  event.remove({ output: 'wstweaks:lava_blade' })
+  event.remove({ output: 'wstweaks:blaze_blade' })
+})
